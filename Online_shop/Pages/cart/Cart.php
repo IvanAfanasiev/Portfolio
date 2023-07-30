4b8d237cbd7255479d0b1041ab99cf7d19abedeb
IvanAfanasiev <138232156+IvanAfanasiev@users.noreply.github.com>
2023-07-30 19:50:19 +0200

    Add files via upload

--- Online_shop/Pages/cart/Cart.php
+++ Online_shop/Pages/cart/Cart.php
@@ -19,10 +19,6 @@ if (!login())
     <title>Document</title>
 </head>
 
-<?php
-require_once "../Header.php";
-?>
-
 
 
 
@@ -47,29 +43,19 @@ require_once "../Header.php";
     <nav class="goodsList">
         <div class='wrapper'>
 
-            <div className='cartContainer'>
-                <div className="cart_wrapper">
-                    <div className="cart_imgContainer">
-                        <img className="cart_cardImageBG" <?php echo "src ='data:image/png;base64," . base64_encode($product['image']) . "' "?> />
-                        <img className="cart_cardImage" <?php echo "src ='data:image/png;base64," . base64_encode($product['image']) . "' "?> />
+            <div class='cardContainer'>
+                <div class='card'>
+                    <div class='imgContainer'>
+                        <img class="cardImageBG" <?php echo "src ='data:image/png;base64," . base64_encode($product['image']) . "' "?> >
+                        <img class="cardImage" src ='<?php echo "data:image/png;base64," . base64_encode($product['image'])?>' >
                     </div>
-                    <div className="cart_cardContainer">
-                        <div className="cart_card">
-                            <div className="cart_platforms">
-                                <ion-icon name="logo-windows"></ion-icon>
-                                <ion-icon name="logo-xbox"></ion-icon>
-                                <ion-icon name="logo-playstation"></ion-icon>
-                            </div>
-                            <div className="cart_description">
-                                <h1><?php echo $product['name'];?></h1>
-                                A brief (or not) description of the game.
-                                Perhaps some other information such as average rating, price or number of downloads
-                            </div>
-                            <div className="Buy">
-                                <div className="price">$14.99</div>
-                                <button className='BuyButtn'>Buy</button>
-                            </div>
+                    <div class="description">
+                        <div class="platforms">
+                            <ion-icon name="logo-windows"></ion-icon>
+                            <ion-icon name="logo-xbox"></ion-icon>
+                            <ion-icon name="logo-playstation"></ion-icon>
                         </div>
+                        <?php echo $product['name']?>
                     </div>
                 </div>
             </div>
@@ -78,12 +64,7 @@ require_once "../Header.php";
     </nav>
 </div>
 
-<?php
-require_once "../Footer.php";
-?>
 
 
-
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </body>
 </html>
