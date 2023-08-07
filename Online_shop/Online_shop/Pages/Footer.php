ba90010a8c360bc767d4bf42889b49d1d98ddd4a
IvanAfanasiev <138232156+IvanAfanasiev@users.noreply.github.com>
2023-08-07 18:35:13 +0200

    Delete Online_shop/Online_shop directory

--- Online_shop/Online_shop/Pages/Footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo <<< FOOTER
-        <footer>
-            <div class="footer">
-                <p>Info or something else (contact details, social networks, advertising)</p>
-            </div>
-        </footer>
-    FOOTER;
--- Online_shop/Online_shop/Pages/LogOut.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['id']); //session variable is deleted
-
-SetCookie("login", "", time() - 5000, '/');
-SetCookie("password", "", time() - 5000, '/');
-
-
-header('Location: ../index.php'); //redirection to the main page of the site
